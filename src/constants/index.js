9d4daf71efb736986ecf99b6e68d14338265de93
Prem Mourya <premmourya998@gmail.com>
2025-01-15 19:35:58 +0530

    My-Portfolio-Professional

---
+++ src/constants/index.js
@@ -0,0 +1,309 @@
+import {
+  mobile,
+  backend,
+  Sql_data_base_with_logo,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  Java,
+  DSA,
+  UI,
+  oops,
+  gemini,
+  codepen,
+  bgapp,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Java",
+    icon: Java,
+  },
+  {
+    title: "Core Java",
+    icon: Java,
+  },
+  {
+    title: "OOPS",
+    icon: oops,
+  },
+  {
+    title: "DSA",
+    icon: DSA,
+  },
+  {
+    title: "SQL",
+    icon: Sql_data_base_with_logo,
+  },
+  {
+    title: "JavaScript",
+    icon: javascript,
+  },
+  {
+    title: "Reactjs",
+    icon: reactjs,
+  },
+  {
+    title: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    title: "UI Design",
+    icon: UI,
+  },
+  {
+    title: "Figma",
+    icon: figma,
+  },
+  {
+    title: "IntelliJ & vscode",
+    icon: backend,
+  },
+  {
+    title: "Version Control",
+    icon: git,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "Java",
+    icon: Java,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  // {
+  //   name: "Node JS",
+  //   icon: nodejs,
+  // },
+  {
+    name: "SQL",
+    icon: Sql_data_base_with_logo,
+  },
+  {
+    name: "Material UI",
+    icon: UI,
+  },
+  {
+    name: "git/git hub",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "Intellij Idea",
+    icon: backend,
+  },
+  {
+    name: "vscode",
+    icon: backend,
+  },
+];
+
+// const experiences = [
+//   {
+//     title: "React.js Developer",
+//     company_name: "Starbucks",
+//     icon: starbucks,
+//     iconBg: "#383E56",
+//     date: "March 2020 - April 2021",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "React Native Developer",
+//     company_name: "Tesla",
+//     icon: tesla,
+//     iconBg: "#E6DEDD",
+//     date: "Jan 2021 - Feb 2022",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "Web Developer",
+//     company_name: "Shopify",
+//     icon: shopify,
+//     iconBg: "#383E56",
+//     date: "Jan 2022 - Jan 2023",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "Full stack Developer",
+//     company_name: "Meta",
+//     icon: meta,
+//     iconBg: "#E6DEDD",
+//     date: "Jan 2023 - Present",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+// ];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Prem proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Prem does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Prem optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "CodePen - Web Based Editor",
+    description:
+      "This web-based code editor is designed for front-end developers to write, test, and showcase their HTML, CSS, and JavaScript code directly in the browser. It offers real-time previews, an intuitive interface, and robust support for modern web development workflows.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "codemirror",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Material UI",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: codepen,
+    source_code_link: "https://github.com/Prem-Mourya/CodePen-webeditor.git",
+  },
+  {
+    name: "Gemini AI Clone",
+    description:
+      "Welcome to Gemini AI Clone—a modern, feature-packed replica of Gemini AI, built with ReactJS ⚛️ and styled with TailwindCSS 💨. This project integrates the Gemini API to deliver powerful AI-driven solutions while maintaining a sleek and responsive design.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Gemini API",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tailwind CSS",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: gemini,
+    source_code_link: "https://github.com/Prem-Mourya/Gemini-Ai-Clone.git",
+  },
+  {
+    name: "Bg Removal web app",
+    description:
+      "This ReactJS-powered web application provides a seamless solution for removing image backgrounds with precision. By leveraging a robust background removal API, the app enables users to upload images and instantly generate background-free versions.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "bg removal api",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: bgapp,
+    source_code_link: "https://github.com/Prem-Mourya/Bg-Removal-webapp.git",
+  },
+];
+
+export { services, technologies, testimonials, projects };
